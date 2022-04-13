e2c4a9f0dea796d8e42fb2e404dd2a2646a60c54
17yie <ayiayiye@gmail.com>
2022-04-13 14:54:33 +0800

    新增商品頁面

---
+++ TreeFriend/TreeFriend/Controllers/LectureController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TreeFriend.Models;
+using TreeFriend.Models.ViewModel;
+
+namespace TreeFriend.Controllers
+{
+    public class LectureController : Controller
+    {
+        private readonly TreeFriendDbContext _db;
+
+
+        public LectureController(TreeFriendDbContext db)
+        {
+            _db = db;
+        }
+        public List<LecturelistViewModel> GetAllLecture()
+        {
+            var result = _db.Lectures.Where(x => x.IsDelete == false).Select(x => new LecturelistViewModel
+            {
+                LectureId = x.LectureId,
+                Theme = x.Theme,
+                EventDate = x.EventDate.ToString("yyyy-MM-dd"),
+                EventTimeStart = x.EventTimeStart.ToString("HH:mm"),
+                EventTimeEnd = x.EventTimeEnd.ToString("HH:mm"),
+                Price = x.Price,
+                Description = x.Description,
+                ImgPath = x.ImgPath
+            }).ToList();
+
+            return result;
+
+        }
+
+        [HttpGet]
+        [Route("[controller]/[action]/{lecturedId}")]
+        public List<LectureDetailViewModel> GetLectureDetails([FromRoute] int lecturedId)
+        {
+            var result = _db.Lectures.Where(x => x.LectureId == lecturedId).Select(x => new LectureDetailViewModel
+            {
+                LectureId = x.LectureId,
+                Theme = x.Theme,
+                EventDate = x.EventDate.ToString("yyyy-MM-dd"),
+                EventTimeStart = x.EventTimeStart.ToString("HH:mm"),
+                EventTimeEnd = x.EventTimeEnd.ToString("HH:mm"),
+                Venue = x.Venue,
+                SpeakerImgPath = x.SpeakerImgPath,
+                Speaker = x.Speaker,
+                Count = x.Count,
+                Price = x.Price,
+                Description = x.Description,
+                Content = x.Content,
+                ImgPath = x.ImgPath
+            }
+            ).ToList();
+
+            return result;
+        }
+    }
+}

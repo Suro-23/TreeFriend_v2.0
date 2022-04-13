4c2d276271099b9f2e073b48da3031d4f5f0d054
17yie <ayiayiye@gmail.com>
2022-04-13 14:27:12 +0800

    新增AddLecture

---
+++ TreeFriend/TreeFriend/Models/ViewModel/AddLecturelistViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TreeFriend.Models.ViewModel
+{
+    public class AddLecturelistViewModel
+    {
+        public int LectureId { get; set; }
+        public string CreateDate { get; set; }
+        public string Theme { get; set; }
+        public string EventDate { get; set; }
+        public string EventTimeStart { get; set; }
+        public string EventTimeEnd { get; set; }
+        public string Venue { get; set; }
+        public string Description { get; set; }
+        public string Content { get; set; }
+        public string SpeakerImgPath { get; set; }
+        public string Speaker { get; set; }
+        public int Count { get; set; }
+        public decimal Price { get; set; }
+        public string ImgPath { get; set; }
+
+        public string UpdateTime { get; set; }
+    }
+}

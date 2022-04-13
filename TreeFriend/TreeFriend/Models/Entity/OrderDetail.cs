ecc954c8fe99d9ac89eca18559e30508c97b4d7e
17yie <ayiayiye@gmail.com>
2022-04-14 01:10:35 +0800

    新增Orderdetail欄位

--- TreeFriend/TreeFriend/Models/Entity/OrderDetail.cs
+++ TreeFriend/TreeFriend/Models/Entity/OrderDetail.cs
@@ -9,15 +9,17 @@ namespace TreeFriend.Models.Entity
     public class OrderDetail
     {
         //訂單編號: 複合主鍵 
-        //TODO藍新金流回傳欄位
+      
         [Required]
         public DateTime CreateDate { get; set; }
         [Required]
-        public decimal Price { get; set; }//單價
+        public decimal Price { get; set; } //單價
         [Required]
         public int Count { get; set; } //買家購買數量
-
-
+        [Required]
+        public DateTime PayTime { get; set; }
+        [Required]
+        public bool PaymentStatus { get; set; } = false;
 
 
         [Required]

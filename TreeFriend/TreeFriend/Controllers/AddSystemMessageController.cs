10fb0ec175d0e7816dbdb37d7bc4b0274cc875fe
TFM104-12 <102593092+TFM104-12@users.noreply.github.com>
2022-04-21 18:33:14 +0800

    新增 系統文章與系統文章後台管理

--- TreeFriend/TreeFriend/Controllers/AddSystemMessageController.cs
+++ TreeFriend/TreeFriend/Controllers/AddSystemMessageController.cs
@@ -23,120 +23,119 @@ namespace TreeFriend.Controllers
             _db = db;
         }
 
-        //#region 渲染後台畫面
-        //    public List<SystemPostViewModel> GetAllMessage()                //查詢資料庫 撈資料後 去渲染畫面
-        //                                                                    //List泛型 復數 的資料
-        //    {
-        //        var result = _db.SystemPost.OrderByDescending(x => x.ArticleID).Take(5)
-        //        .Where(x => x.IsDelete == false).Select(x => new SystemPostViewModel()       // 先抓取資料 在改寫資料庫欄位 new一個新的Model
-        //        {
-        //            ArticleID = x.ArticleID,
-        //            CreateDate = x.CreateDate.ToString("yyyy-MM-dd"),
-        //            Title = x.Title,
-        //            Description = x.Description,
-        //            PicPath = "/SystemPicture/" + x.PicPath  //對應資料庫欄位
+        #region 渲染後台畫面
+        public List<SystemPostViewModel> GetAllMessage()                //查詢資料庫 撈資料後 去渲染畫面
+                                                                        //List泛型 復數 的資料
+        {
+            var result = _db.SystemPost.OrderByDescending(x => x.ArticleID).Take(5)
+            .Where(x => x.IsDelete == false).Select(x => new SystemPostViewModel()       // 先抓取資料 在改寫資料庫欄位 new一個新的Model
+                {
+                ArticleID = x.ArticleID,
+                CreateDate = x.CreateDate.ToString("yyyy-MM-dd"),
+                Title = x.Title,
+                Description = x.Description,
+                PicPath = "/SystemPicture/" + x.PicPath  //對應資料庫欄位
 
-        //        }).ToList();
+                }).ToList();
 
-        //        return result;
-        //    }
-        //    #endregion
+            return result;
+        }
+        #endregion
 
-        //    #region 系統頁面新增
-        //    [HttpPost]
-        //    public bool CreateMessage(UploadFileMessageViewModel model) //前端送過來的資料 用view model來接
-        //                                                                //建議自己在寫一個 不要參照資料庫的欄位
-        //    {
-        //        var UserId = Convert.ToInt32(HttpContext.User.Claims.FirstOrDefault(u => u.Type == "UserId").Value);
-        //        var path = _environment.WebRootPath + "/SystemPicture";
+        #region 系統頁面新增
+        [HttpPost]
+        public bool CreateMessage(UploadFileMessageViewModel model) //前端送過來的資料 用view model來接
+                                                                    //建議自己在寫一個 不要參照資料庫的欄位
+        {
+            var UserId = Convert.ToInt32(HttpContext.User.Claims.FirstOrDefault(u => u.Type == "UserId").Value);
+            var path = _environment.WebRootPath + "/SystemPicture";
 
-        //        //前端資料確定有送進來後 開始寫傳照片的事件
-        //        var picture = model.PicPath.FirstOrDefault();
-        //        if (picture != null)
-        //        {
-        //            var fileName = DateTime.Now.Ticks.ToString() + picture.FileName; //為了避免傳同一張照片 給他時間做區別
+            //前端資料確定有送進來後 開始寫傳照片的事件
+            var picture = model.PicPath.FirstOrDefault();
+            if (picture != null)
+            {
+                var fileName = DateTime.Now.Ticks.ToString() + picture.FileName; //為了避免傳同一張照片 給他時間做區別
 
-        //            using (var files = System.IO.File.Create($"{path}/{fileName}"))  //開啟檔案位置 {路徑}+{檔案名稱}
-        //            {
-        //                picture.CopyTo(files);  //圖片物件載完後 可以CopyTo 丟入另一個路徑地方
-        //            }
+                using (var files = System.IO.File.Create($"{path}/{fileName}"))  //開啟檔案位置 {路徑}+{檔案名稱}
+                {
+                    picture.CopyTo(files);  //圖片物件載完後 可以CopyTo 丟入另一個路徑地方
+                }
 
-        //            _db.SystemPost.Add(new SystemPost()
-        //            {
-        //                Title = model.Title,
-        //                Description = model.Description,
-        //                CreateDate = DateTime.Now,
-        //                PicPath = fileName,
-        //                UserId = model.UserId
-        //            });
-        //            _db.SaveChanges(); //存進資料庫
-        //            return true;
-        //        }
-        //        else
-        //        {
-        //            return false;
-        //        }
-        //    }
-        //    #endregion
+                _db.SystemPost.Add(new SystemPost()
+                {
+                    Title = model.Title,
+                    Description = model.Description,
+                    CreateDate = DateTime.Now,
+                    PicPath = fileName,
+                    UserId = model.UserId
+                });
+                _db.SaveChanges(); //存進資料庫
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
 
-        //    #region 系統頁面修改
-        //    [HttpPost]
-        //    public bool UpdateMessage([FromForm] UpdateMessageViewModel model)
-        //    {
-        //        var updateMessage = _db.SystemPost.FirstOrDefault(x => x.ArticleID == model.ArticleID);
-        //        var path = _environment.WebRootPath + "/SystemPicture";
+        #region 系統頁面修改
+        [HttpPost]
+        public bool UpdateMessage([FromForm] UpdateMessageViewModel model)
+        {
+            var updateMessage = _db.SystemPost.FirstOrDefault(x => x.ArticleID == model.ArticleID);
+            var path = _environment.WebRootPath + "/SystemPicture";
 
-        //        string PicPath;
+            string PicPath;
 
-        //        if (model.PicPath == null)
-        //        {
-        //            PicPath = updateMessage.PicPath;
-        //        }
-        //        else
-        //        {
-        //            var fileName = DateTime.Now.Ticks.ToString("MMddHHmmss") + model.PicPath[0].FileName;
-        //            using (var files = System.IO.File.Create($"{path}/{fileName}"))
-        //            {
-        //                model.PicPath[0].CopyTo(files);
-        //                PicPath = fileName;
-        //            }
-        //        }
+            if (model.PicPath == null)
+            {
+                PicPath = updateMessage.PicPath;
+            }
+            else
+            {
+                var fileName = DateTime.Now.Ticks.ToString("MMddHHmmss") + model.PicPath[0].FileName;
+                using (var files = System.IO.File.Create($"{path}/{fileName}"))
+                {
+                    model.PicPath[0].CopyTo(files);
+                    PicPath = fileName;
+                }
+            }
 
 
-        //        if (updateMessage != null)
-        //        {
-        //            updateMessage.Title = model.Title;
-        //            updateMessage.Description = model.Description;
-        //            updateMessage.PicPath = PicPath;
-        //            _db.SaveChanges();
-        //            return true;
-        //        }
-        //        else
-        //        {
-        //            return false;
-        //        }
-        //    }
-        //    #endregion
+            if (updateMessage != null)
+            {
+                updateMessage.Title = model.Title;
+                updateMessage.Description = model.Description;
+                updateMessage.PicPath = PicPath;
+                _db.SaveChanges();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        #endregion
 
-        //    #region 系統頁面軟刪除
-        //    public bool DeleteMessage([FromQuery] int ArticleID)
-        //    {
-        //        try
-        //        {
-        //            var result = _db.SystemPost.Where(x => x.ArticleID == ArticleID).SingleOrDefault();
-        //            Console.WriteLine(result);
-        //            result.IsDelete = true;
-        //            _db.SystemPost.Update(result);
-        //            _db.SaveChanges();
-        //            return true;
-        //        }
-        //        catch (Exception)
-        //        {
-        //            return false;
-        //        }
-        //    }
+        #region 系統頁面軟刪除
+        public bool DeleteMessage([FromQuery] int ArticleID)
+        {
+            try
+            {
+                var result = _db.SystemPost.Where(x => x.ArticleID == ArticleID).SingleOrDefault();
+                Console.WriteLine(result);
+                result.IsDelete = true;
+                _db.SystemPost.Update(result);
+                _db.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
 
-        //    #endregion
-        //}
+        #endregion
     }
 }
--- TreeFriend/TreeFriend/Controllers/SystemMessageController.cs
+++ TreeFriend/TreeFriend/Controllers/SystemMessageController.cs
@@ -19,35 +19,35 @@ namespace TreeFriend.Controllers
             _db = db;
         }
 
-        //[AllowAnonymous]
-        //public List<SystemlistViewModel> GetAllSystemMessage() //渲染系統文章頁面
-        //{
-        //    var result = _db.SystemPost.OrderByDescending(x => x.ArticleID).Take(5)
-        //     .Where(x => x.IsDelete == false).Select(x => new SystemlistViewModel()         // 先抓取資料 在改寫資料庫欄位 new一個新的Model
-        //     {
-        //         ArticleID = x.ArticleID,
-        //         CreateDate = x.CreateDate.ToString("yyyy-MM-dd"),
-        //         Title = x.Title,
-        //         Description = x.Description,
-        //         PicPath = "/SystemPicture/" + x.PicPath  //對應資料庫欄位
+        [AllowAnonymous]
+        public List<SystemlistViewModel> GetAllSystemMessage() //渲染系統文章頁面
+        {
+            var result = _db.SystemPost.OrderByDescending(x => x.ArticleID).Take(5)
+             .Where(x => x.IsDelete == false).Select(x => new SystemlistViewModel()         // 先抓取資料 在改寫資料庫欄位 new一個新的Model
+             {
+                 ArticleID = x.ArticleID,
+                 CreateDate = x.CreateDate.ToString("yyyy-MM-dd"),
+                 Title = x.Title,
+                 Description = x.Description,
+                 PicPath = "/SystemPicture/" + x.PicPath  //對應資料庫欄位
 
-        //     }).ToList();
+             }).ToList();
 
-        //    return result;
+            return result;
 
-        //}
+        }
 
-        //[HttpGet]
-        //public SystemPost FullContent1(int articleId)
-        //{
-        //    var post = _db.SystemPost.FirstOrDefault(x => x.ArticleID == articleId);
-        //    var systemPost = new SystemPost();
-        //    systemPost.ArticleID = post.ArticleID;
-        //    systemPost.Title = post.Title;
-        //    systemPost.Description = post.Description;
-        //    systemPost.PicPath = "/SystemPicture/" + post.PicPath;
+        [HttpGet]
+        public SystemPost FullContent1(int articleId)
+        {
+            var post = _db.SystemPost.FirstOrDefault(x => x.ArticleID == articleId);
+            var systemPost = new SystemPost();
+            systemPost.ArticleID = post.ArticleID;
+            systemPost.Title = post.Title;
+            systemPost.Description = post.Description;
+            systemPost.PicPath = "/SystemPicture/" + post.PicPath;
 
-        //    return systemPost;
-        //}
+            return systemPost;
+        }
     }
 }
